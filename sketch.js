68cb76ffac6a60abe2ac74a407a5b3c91a7b0199
srijesh2811 <107938337+srijesh2811@users.noreply.github.com>
2023-01-27 16:35:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,229 +1,134 @@
 const Engine = Matter.Engine;
-const Render = Matter.Render;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
-const Body = Matter.Body;
-const Composites = Matter.Composites;
-const Composite = Matter.Composite;
 
-let engine;
-let world;
-var rope,fruit,ground;
-var fruit_con;
-var fruit_con_2;
-var fruit_con_3;
-var rope3;
-
-var bg_img;
-var food;
-var rabbit;
-
-var button,button2,button3;
-var bunny;
-var blink,eat,sad;
-var mute_btn;
-
-var fr;
-
-var bk_song;
-var cut_sound;
-var sad_sound;
-var eating_sound;
-var air;
-var canW;
-var canH;
-
-function preload()
-{
-  bg_img = loadImage('background.png');
-  food = loadImage('melon.png');
-  rabbit = loadImage('Rabbit-01.png');
-
-  bk_song = loadSound('sound1.mp3');
-  sad_sound = loadSound("sad.wav")
-  cut_sound = loadSound('rope_cut.mp3');
-  eating_sound = loadSound('eating_sound.mp3');
-  air = loadSound('air.wav');
-
-  blink = loadAnimation("blink_1.png","blink_2.png","blink_3.png");
-  eat = loadAnimation("eat_0.png" , "eat_1.png","eat_2.png","eat_3.png","eat_4.png");
-  sad = loadAnimation("sad_1.png","sad_2.png","sad_3.png");
   
-  blink.playing = true;
-  eat.playing = true;
-  sad.playing = true;
-  sad.looping= false;
-  eat.looping = false; 
+
+var engine, world;
+var canvas;
+var palyer, playerBase, playerArcher;
+var playerArrows = [];
+var board1, board2;
+var numberOfArrows = 10;
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
 }
 
-function setup() 
-{
-  
-  var isMobile= /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
-  if(isMobile){
-    canW= displayWidth
-    canH= displayHeight
-    createCanvas(displayWidth+80,displayHeight)
-    
-  } 
-  else{
-    canW=windowWidth
-    canH=windowHeight
-    createCanvas(windowWidth,windowHeight)
-  }
-
-  
-  frameRate(80);
-
-  bk_song.play();
-  bk_song.setVolume(0.5);
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
 
   engine = Engine.create();
   world = engine.world;
 
-  //btn 1
-  button = createImg('cut_btn.png');
-  button.position(20,30);
-  button.size(50,50);
-  button.mouseClicked(drop);
+  playerBase = new PlayerBase(300, 500, 180, 150);
+  player = new Player(285, playerBase.body.position.y - 153, 50, 180);
+  playerArcher = new PlayerArcher(
+    340,
+    playerBase.body.position.y - 180,
+    120,
+    120
+  );
 
-   //btn 2
-   button2 = createImg('cut_btn.png');
-   button2.position(330,35);
-   button2.size(60,60);
-   button2.mouseClicked(drop2);
- 
-   //btn3
-   button3 = createImg('cut_btn.png');
-   button3.position(360,200);
-   button3.size(60,60);
-   button3.mouseClicked(drop3);
-
-  mute_btn = createImg('mute.png');
-  mute_btn.position(450,20);
-  mute_btn.size(50,50);
-  mute_btn.mouseClicked(mute);
-  
-  rope = new Rope(8,{x:40,y:30});
-  rope2 = new Rope(7,{x:370,y:40});
-  rope3 = new Rope(4,{x:400,y:225});
-
-  ground = new Ground(200,canH,600,20);
-  blink.frameDelay = 20;
-  eat.frameDelay = 20;
-
-  bunny = createSprite(170,canH-80,100,100);
-  bunny.scale = 0.2;
-
-  bunny.addAnimation('blinking',blink);
-  bunny.addAnimation('eating',eat);
-  bunny.addAnimation('crying',sad);
-  bunny.changeAnimation('blinking');
-  
-  fruit = Bodies.circle(300,300,20);
-  Matter.Composite.add(rope.body,fruit);
-
-  fruit_con = new Link(rope,fruit);
-  fruit_con_2 = new Link(rope2,fruit);
-  fruit_con_3 = new Link(rope3,fruit);
-
-  rectMode(CENTER);
-  ellipseMode(RADIUS);
-  textSize(50)
-  
+  board1 = new Board(width - 300, 330, 50, 200);
+  board2 = new Board(width - 550, height - 300, 50, 200);
 }
 
-function draw() 
-{
-  background(51);
-  image(bg_img,0,0,displayWidth+80, displayHeight);
-
-  push();
-  imageMode(CENTER);
-  if(fruit!=null){
-    image(food,fruit.position.x,fruit.position.y,70,70);
-  }
-  pop();
-
-  rope.show();
-  rope2.show();
-  rope3.show();
+function draw() {
+  background(backgroundImg);
 
   Engine.update(engine);
-  ground.show();
 
-  drawSprites();
+  playerBase.display();
+  player.display();
+  playerArcher.display();
 
-  if(collide(fruit,bunny)==true)
-  {
-    bunny.changeAnimation('eating');
-    eating_sound.play();
+  board1.display();
+  board2.display();
+
+  for (var i = 0; i < playerArrows.length; i++) {
+    if (playerArrows[i] !== undefined) {
+      playerArrows[i].display();
+    }
   }
 
-  if(fruit!=null && fruit.position.y>=650)
-  {
-    bunny.changeAnimation('crying');
-    bk_song.stop();
-    sad_sound.play();
-    fruit=null;
-     
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+
+  // Arrow Count
+  fill("#FFFF");
+  textAlign("center");
+  textSize(30);
+  text("Remaining Arrows : " + numberOfArrows, 200, 100);
+}
+
+function keyPressed() {
+  // if (keyCode === 23) {
+  //   if (numberOfArrows > 0) {
+  //     var posX = playerArcher.body.position.x;
+  //     var posY = playerArcher.body.position.y;
+  //     var angle = playerArcher.body.angle;
+
+  //     var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
+
+  //     Matter.Body.setAngle(arrow.body, angle);
+  //     playerArrows.push(arrow);
+  //     numberOfArrows -= 1;
+  //   }
+  // }
+
+   // if (keyCode === 32) {
+  //   if (numberOfArrows > 0) {
+  //     var posX = playerArcher.body.position.x;
+  //     var posY = playerArcher.body.position.y;
+  //     var angle = playerArcher.body.angle;
+
+  //     var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
+
+  //     Matter.Body.setAngle(arrow.body, angle);
+  //     playerArrows.push(arrow);
+  //     numberOfArrows += 1;
+  //   }
+  // }
+
+    if (keyCode === 32) {
+     if (numberOfArrows > 0) {
+       var posX = playerArcher.body.position.x;
+       var posY = playerArcher.body.position.y;
+       var angle = playerArcher.body.angle;
+
+       var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
+
+       Matter.Body.setAngle(arrow.body, angle);
+       playerArrows.push(arrow);
+       numberOfArrows -= 1;
+     }
    }
-   
+
+   // if (keyCode === 32) {
+  //   if (numberOfArrows > 0) {
+  //     var posX = playerArcher.body.position.x;
+  //     var posY = playerArcher.body.position.y;
+  //     var angle = playerArcher.body.angle;
+
+  //     var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
+
+  //     Matter.Body.setAngle(arrow.body, angle);
+  //     playerArrows.push(arrow);
+  //     numberOfArrows *= 1;
+  //   }
+  // }
 }
 
-function drop()
-{
-  cut_sound.play();
-  rope.break();
-  fruit_con.detach();
-  fruit_con = null; 
+function keyReleased() {
+  if (keyCode === 32) {
+    if (playerArrows.length) {
+      var angle = playerArcher.body.angle;
+      playerArrows[playerArrows.length - 1].shoot(angle);
+    }
+  }
 }
-
-function drop2()
-{
-  cut_sound.play();
-  rope2.break();
-  fruit_con_2.detach();
-  fruit_con_2 = null;
-}
-
-function drop3()
-{
-  cut_sound.play();
-  rope3.break();
-  fruit_con_3.detach();
-  fruit_con_3 = null;
-}
-
-
-function collide(body,sprite)
-{
-  if(body!=null)
-        {
-         var d = dist(body.position.x,body.position.y,sprite.position.x,sprite.position.y);
-          if(d<=80)
-            {
-              World.remove(engine.world,fruit);
-               fruit = null;
-               return true; 
-            }
-            else{
-              return false;
-            }
-         }
-}
-
-
-function mute()
-{
-  if(bk_song.isPlaying())
-     {
-      bk_song.stop();
-     }
-     else{
-      bk_song.play();
-     }
-}
-
-
